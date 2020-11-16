5cc55bc5f9b6d24d1ef17292515a470dca4d6e5a
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-16 20:17:34 -0300

    Adding list and navigaton to the drawer menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <nav>
     <v-toolbar flat app>
-      <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        class="grey--text"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
       <v-toolbar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
@@ -13,8 +16,17 @@
       </v-btn>
     </v-toolbar>
 
-    <v-navigation-drawer  temporary app v-model="drawer" class="primary">
-      <p>Test</p>
+    <v-navigation-drawer temporary app v-model="drawer" class="primary">
+      <v-list>
+        <v-list-item v-for="link in links" :key="link.text" :to="link.route">
+          <v-list-item-action> 
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{link.text}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
   </nav>
 </template>
@@ -24,6 +36,11 @@ export default {
   data() {
     return {
       drawer: false,
+      links: [
+        { icon: "mdi-view-dashboard", text: "Dashboard", route: "/" },
+        { icon: "mdi-folder", text: "My Projects", route: "/projects" },
+        { icon: "mdi-account", text: "Team", route: "/team" },
+      ],
     };
   },
 };
