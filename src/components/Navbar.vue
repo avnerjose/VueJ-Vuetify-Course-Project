7f8cf259f06e8cd5c6872db8cce8ea156eb7555b
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-16 18:17:03 -0300

    Adding drawer navigator

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,33 @@
 <template>
   <nav>
-      <v-toolbar flat app>
-        <v-toolbar-title class="text-uppercase grey--text">
-            <span class="font-weight-light">Todo</span>
-            <span>Ninja</span>  
-        </v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn text color="grey">
-            <span>Sign out</span>
-            <v-icon right>mdi-exit-to-app</v-icon>
-        </v-btn>
-      </v-toolbar>
+    <v-toolbar flat app>
+      <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title class="text-uppercase grey--text">
+        <span class="font-weight-light">Todo</span>
+        <span>Ninja</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text color="grey">
+        <span>Sign out</span>
+        <v-icon right>mdi-exit-to-app</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer  temporary app v-model="drawer" class="indigo">
+      <p>Test</p>
+    </v-navigation-drawer>
   </nav>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
