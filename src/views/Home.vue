6866871c0d5ee50d63d6a6a7f5a0f08c88bb218c
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-16 18:03:03 -0300

    Deleting uncessessary code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <v-btn  class="hidden-md-and-down">Click me</v-btn>
-    <v-btn class="hidden-md-and-up">Click me</v-btn>
-    <v-btn class="hidden-sm-only">Click me</v-btn>
+   
   </div>
 </template>
 
