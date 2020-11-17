57c46ebb4c0476d7ba611f6e8f31ff8c85883d0a
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-17 09:48:27 -0300

    Using grid system to create dashboard card layout

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,20 +2,26 @@
   <div class="home">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-row no-gutters>
-        <v-col cols="12" md="6" class="mb-4">
-          <v-btn outlined block color="primary">1</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-        <v-col cols="4" md="2">
-          <v-btn outlined block color="primary">2</v-btn>
-        </v-col>
-      </v-row>
+      <v-card flat class="pa-3">
+        <v-row>
+          <v-col cols="12" md="6">
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>The Net Ninja</div>
+          </v-col>
+          <v-col cols="6" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2019</div>
+          </v-col>
+          <v-col cols="3" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>Ongoing</div>
+          </v-col>
+        </v-row>
+      </v-card>
     </v-container>
   </div>
 </template>
