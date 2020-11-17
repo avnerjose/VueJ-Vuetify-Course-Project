8c3d81cbebd9c945d0e0289a75549cb3c01779e2
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-17 09:33:22 -0300

    Learning grid system

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="home">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
-    <v-container class="my-5"> content </v-container>
+    <v-container class="my-5">
+      <v-row no-gutters>
+        <v-col cols="12" md="6" class="mb-4">
+          <v-btn outlined block color="primary">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+        <v-col cols="4" md="2">
+          <v-btn outlined block color="primary">2</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
