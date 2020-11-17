0c59e5530cbd122188050e944ea1e59405389f74
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-17 10:13:35 -0300

    Adding data and styles in dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,24 +2,32 @@
   <div class="home">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-card flat class="pa-3">
-        <v-row>
+      <v-card
+        flat
+        class="px-3"
+        v-for="project in projects"
+        :key="project.title"
+      >
+        <v-row :class="`pl-3 project ${project.status}`">
           <v-col cols="12" md="6">
-            <div class="caption grey--text">Project title</div>
-            <div>Create a new website</div>
+            <div class="caption grey--text">Project Title</div>
+            <div>{{ project.title }}</div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
             <div class="caption grey--text">Person</div>
-            <div>The Net Ninja</div>
+            <div>{{ project.person }}</div>
           </v-col>
           <v-col cols="6" sm="4" md="2">
             <div class="caption grey--text">Due by</div>
-            <div>1st Jan 2019</div>
+            <div>{{ project.due }}</div>
           </v-col>
           <v-col cols="3" sm="4" md="2">
             <div class="caption grey--text">Status</div>
-            <div>Ongoing</div>
+            <div>{{ project.status }}</div>
           </v-col>
+        </v-row>
+        <v-row cols="12">
+          <v-divider></v-divider>
         </v-row>
       </v-card>
     </v-container>
@@ -28,11 +36,56 @@
 
 <script>
 // @ is an alias to /src
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "The Net Ninja",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Code up the homepage",
+          person: "Chun Li",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Ryu",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+        {
+          title: "Create a community forum",
+          person: "Gouken",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style scoped>
-button {
-  margin-left: 16px;
+<style >
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+.project.overdue {
+  border-left: 4px solid tomato;
 }
 </style>
