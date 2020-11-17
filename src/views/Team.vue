c71bd277f8f4966a6e797f0245ecc9cc2a6eaf1e
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-17 16:35:39 -0300

    Adding cards to the team view

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,12 +1,44 @@
 <template>
   <div class="team">
     <h1 class="subtitle-1 grey--text">Team</h1>
-    <v-container class="my-5"> content </v-container>
+    <v-container class="my-5">
+      <v-row>
+        <v-col cols="12" sm="6" md="4" lg="3" v-for="person in team" :key="person.name">
+          <v-card flat class="text-center ma-3">
+            <v-responsive class="pt-4">
+              image goes here
+            </v-responsive>
+            <v-card-text>
+              <div class="subtitle-3">{{person.name}}</div>
+              <div class="grey--text">{{person.role}}</div>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="grey">
+                <v-icon small left>mdi-message</v-icon>
+                <span>Message</span>
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      team: [
+        { name: "The Net Ninja", role: "Web developer" },
+        { name: "Ryu", role: "Graphic designer" },
+        { name: "Chun Li", role: "Web developer" },
+        { name: "Gouken", role: "Social media maverick" },
+        { name: "Yoshi", role: "Sales guru" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
