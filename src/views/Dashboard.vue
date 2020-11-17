9c4ce10c2d1616c8b7468a97174b8fadd09d8f48
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-17 14:31:22 -0300

    Sorting project items

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,18 @@
   <div class="home">
     <h1 class="subtitle-1 grey--text">Dashboard</h1>
     <v-container class="my-5">
+
+      <v-row class="mb-3">
+          <v-btn small text color="grey" @click="sortBy('title')">
+            <v-icon left small>mdi-folder</v-icon>
+            <span class="caption text--lowercase">By project name</span>
+          </v-btn>
+          <v-btn small text color="grey" @click="sortBy('person')">
+            <v-icon left small>mdi-account</v-icon>
+            <span class="caption text--lowercase">By person</span>
+          </v-btn>
+      </v-row>
+
       <v-card
         flat
         class="px-3"
@@ -81,6 +93,11 @@ export default {
       ],
     };
   },
+  methods:{
+    sortBy(prop){
+        this.projects.sort((a,b)=>a[prop] < b[prop] ? -1 : 1)
+    }
+  }
 };
 </script>
 
