a4db997181a0e04545e824a1ab34064562c822a6
avnerjose <49520658+avnerjose@users.noreply.github.com>
2020-11-17 19:18:45 -0300

    Adding dropdown menu to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,11 +5,28 @@
         class="grey--text"
         @click="drawer = !drawer"
       ></v-app-bar-nav-icon>
-      <v-toolbar-title class="text-uppercase grey--text">
+      <lv-toobar-title class="text-uppercase grey--text">
         <span class="font-weight-light">Todo</span>
         <span>Ninja</span>
-      </v-toolbar-title>
+      </lv-toobar-title>
       <v-spacer></v-spacer>
+
+      <v-menu offset-y>
+        <template v-slot:activator="{on}">
+          <v-btn text slot="activator" color="grey" v-on="on">
+            <v-icon left>mdi-menu-down</v-icon>
+            <span>Menu</span>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item v-for="link in links" :key="link.text" :to="link.route">
+            <v-list-item-title>
+              {{ link.text }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+
       <v-btn text color="grey">
         <span>Sign out</span>
         <v-icon right>mdi-exit-to-app</v-icon>
@@ -20,11 +37,9 @@
       <v-row justify="center">
         <v-col cols="6" class="mt-5">
           <v-avatar size="100">
-            <img src="avatar-1.png" alt="">
+            <img src="avatar-1.png" alt="" />
           </v-avatar>
-          <p class="white--text subtitle-3 mt-1">
-            The Net Ninja
-          </p>
+          <p class="white--text subtitle-3 mt-1">The Net Ninja</p>
         </v-col>
       </v-row>
       <v-list>
